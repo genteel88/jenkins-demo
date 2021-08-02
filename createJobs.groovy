5d3f922fffcbb8c709738b784db2afdc5383bd2b
genteel88 <hubiao1688@gmail.com>
2021-08-01 20:20:57 -0500

    first commit

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,32 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/genteel88/spring-boot-api-example.git'
+                    }
+                    branch 'main'
+                }
+            }
+        }
+    }
+}
+pipelineJob('theme-park-job-docker') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/genteel88/spring-boot-api-example.git'
+                    }
+                    branch 'main'
+                    scriptPath 'Jenkinsfile-docker'
+                }
+            }
+        }
+    }
+}
